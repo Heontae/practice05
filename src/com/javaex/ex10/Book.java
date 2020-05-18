e3523de07c0fad3faae7187e753ae6616bcff543
'booldook' <'gjsxo103@naver.com'>
2020-05-18 16:06:32 +0900

    2

--- src/com/javaex/ex10/Book.java
+++ src/com/javaex/ex10/Book.java
@@ -6,15 +6,15 @@ public class Book {
 	String author; // 작가
 	int stateCode; // 상태코드(대여유무를 나타내는 코드)
 
-	//생성자
+	// 생성자
 	Book(int bookNo, String title, String author) {
 		this.bookNo = bookNo;
 		this.title = title;
 		this.author = author;
-		this.stateCode=1;
+		this.stateCode = 1;
 	}
-	
-	//g/s
+
+	// g/s
 	public int getBookNo() {
 		return bookNo;
 	}
@@ -46,20 +46,27 @@ public class Book {
 	public void setStateCode(int stateCode) {
 		this.stateCode = stateCode;
 	}
-	
+
 	public void rent() {
-		this.stateCode=0;
-		if(stateCode==0) {
-			System.out.println(title+"이(가) 대여 됐습니다.");
+		this.stateCode = 0;
+		if (stateCode == 0) {
+			System.out.println(title + "이(가) 대여 됐습니다.");
 		}
 	}
-	//출력문
+
+	// 출력문
 	public void print() {
-		if(stateCode==1){
-			System.out.println(getBookNo()+"책 제목: "+title+", 작가:"+author+", 대여 유무:재고있음");
-		}
-		else if(stateCode==0) {
-			System.out.println(bookNo+"책 제목: "+title+", 작가:"+author+", 대여 유무:대여중");
+		if (stateCode == 1) {
+			System.out.println(getBookNo() + "책 제목: " + title + ", 작가:" + author + ", 대여 유무:재고있음");
+		} else if (stateCode == 0) {
+			System.out.println(bookNo + "책 제목: " + title + ", 작가:" + author + ", 대여 유무:대여중");
 		}
 	}
+	// to String
+
+	@Override
+	public String toString() {
+		return "Book [bookNo=" + bookNo + ", title=" + title + ", author=" + author + ", stateCode=" + stateCode + "]";
+	}
+	
 }
